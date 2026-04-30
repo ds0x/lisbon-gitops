3df9e3d331795504419d410d3f461f44bd4248a0
Dave Siederer (ds) <113131122+ds0x@users.noreply.github.com>
2026-04-30 14:45:47 -0400

    Add user alert script for macOS

---
+++ lib/macos/software/user-alert-s2.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Original code from Armin Briegel - Scripting OS X
+# https://scriptingosx.com/2020/08/running-a-command-as-another-user/
+
+# variable and function declarations
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+# get the currently logged in user
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+# global check if there is a user logged in
+if [ -z "$currentUser" -o "$currentUser" = "loginwindow" ]; then
+  echo "no user logged in, cannot proceed"
+  exit 1
+fi
+
+uid=$(id -u "$currentUser")
+
+runAsUser() {  
+  if [ "$currentUser" != "loginwindow" ]; then
+    launchctl asuser "$uid" sudo -u "$currentUser" "$@"
+  else
+    echo "no user logged in"
+    # uncomment the exit command
+    # to make the function exit with an error when no user is logged in
+    # exit 1
+  fi
+}
+
+# The command to run as logged-in user.
+runCommand=$(osascript -e "display dialog \"Alert delivered to $currentUser, UID $uid\"")
+
+# Runs the command as the GUI logged-in user using the subroutine.
+runAsUser "$runCommand"
+
+# Runs the command as the script running user with no consideration.
+# sh "$runCommand"
